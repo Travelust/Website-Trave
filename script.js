1284072ca55a7aa090f0154e3e581fe5c0edf0b8
Travelust <161230198+Travelust@users.noreply.github.com>
2024-02-25 22:58:54 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const container = document.querySelector(".container");
+const jumbo = document.querySelector(".jumbo");
+const tumbs = document.querySelectorAll(".thumb");
+container.addEventListener("click", function (e) {
+  if (e.target.className == "thumb") {
+    jumbo.src = e.target.src;
+    jumbo.classList.add("fade");
+    setTimeout(function () {
+      jumbo.classList.remove("fade");
+    }, 500);
+    tumbs.forEach(function (thumb) {
+      //   if (thumb.classList.contains("active")) {
+      //     thumb.classList.remove("active");
+      //   }
+      thumb.className = "thumb";
+    });
+    e.target.classList.add("active");
+  }
+});
